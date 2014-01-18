c10044e6ab7c1b1d4a5b9e27e75ac7fb02329f8b
savanto <savanto@gmail.com>
2014-01-18 13:18:45 -0800

    Problem 053: Combinatoric selections.

---
+++ 053.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Combinatoric selections.
+Problem 053
+
+There are exactly ten ways of selecting three from five, 12345:
+
+  123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+In combinatorics, we use the notation 5C3 = 10.
+
+In general,
+
+  nCr = n!/(r!(n-r)!), where r<=n, n! = n*(n-1)* ... *3*2*1, and 0! = 1.
+
+It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+
+How many, not necessarily distinct, values of nCr, for 1 <= n <= 100, are
+greater than one-million?
+"""
+
+from math import factorial
+
+count = 0
+for n in range(1, 101):
+  for r in range(1, n+1):
+    if factorial(n) / (factorial(r) * factorial(n - r)) > 1000000:
+      count += 1
+print(count, "values of nCr are > 1000000 for 1 <= n <= 100.")
