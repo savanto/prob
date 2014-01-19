bab95290a94642d7b7baa856a39ba4862920e10a
savanto <savanto@gmail.com>
2014-01-18 17:27:39 -0800

    Problem 057: Square root convergents.

---
+++ 057.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+Square root convergents.
+Problem 057
+
+It is possible to show that the square root of two can be expressed as an
+infinite continued fraction.
+
+  sqrt(2) = 1 + 1/(2 + 1/(2 + 1/(2 + ...))) = 1.414213...
+
+By expanding this for the first four iterations, we get:
+
+  1 + 1/2 = 3/2 = 1.5
+  1 + 1/(2 + 1/2) = 7/5 = 1.4
+  1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+  1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+The next three expansions are 99/70, 239/169, and 577/408, but the eighth
+expansion, 1393/985, is the first example where the number of digits in the
+numerator exceeds the number of digits in the denominator.
+
+In the first one-thousand expansions, how many fractions contain a numerator
+with more digits than denominator?
+"""
+
+# Calculate 1000th expansion
+n = 1
+d = 2
+for i in range(999):
+  tmp = n + d * 2
+  n = d
+  d = tmp
+n += d
+
+# Reverse the process, checking lengths of n vs d
+count = 0
+while (n != 1):
+  if len(str(n)) > len(str(d)):
+    count += 1
+  tmp = n - d
+  n = d - tmp
+  d = tmp
+print(count, "fractions have longer n than d.")
