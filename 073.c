28bcfe8b005630dd0f259c84e645c6ea978d45a0
savanto <savanto@gmail.com>
2014-03-19 00:27:40 -0700

    Problem 073: Counting fractions in a range.

---
+++ 073.c
@@ -0,0 +1,42 @@
+/* Counting fractions in a range.
+ * Problem 073
+ *
+ * Consider the fraction, n/d, where n and d are positive integers. If n < d
+ * and HCF(n, d) = 1, it is called a reduced proper fraction.
+ *
+ * If we list the set of reduced proper fractions for d <= 8 in ascending order
+ * of size, we get:
+ *
+ *    1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3,
+ *      5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+ *
+ * It can be seen that there are 3 fractions between 1/3 and 1/2.
+ *
+ * How many fractions lie between 1/3 and 1/2 in the sorted set of reduced
+ * proper fractions for d <= 12,000?
+ */
+
+#include <stdio.h>
+
+#define D 12000
+
+int gcd(int n, int d) {
+  if (n == 0)
+    return d;
+  return gcd(d % n, n);
+}
+
+int main() {
+  int n, d;
+  unsigned long long count = 0;
+  for (d = 5; d <= D; ++d) {
+    n = (d - 1) / 2;
+    for (; 3 * n > 1 * d; --n)
+      if (gcd(n, d) == 1)
+        ++count;
+  }
+
+  printf("Reduced proper fractions between 1/3 and 1/2 for d <= %d: %llu\n", 
+      D, count);
+  return 0;
+}
